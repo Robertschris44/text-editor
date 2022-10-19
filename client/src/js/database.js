7679ffc2ce87bbde2335fcfa717256cb296d883b
Chris Key <robertschris010@gmail.com>
2022-10-18 21:04:37 -0500

    finished database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,6 +24,18 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('get from db');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+  ? console.log('content found', result.value)
+  : console.log('not found');
+
+  return result?.value;
+};
 
 initdb();
